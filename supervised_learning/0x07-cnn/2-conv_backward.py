989d5f91b7da947ac66d180454675aba3ac27cb9
alejogonza <alejo.1996.2001@gmail.com>
2020-07-07 11:20:59 -0500

    [fix] datas

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -9,21 +9,6 @@ z
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """Performs back propagation over a convolutional layer of a neural
     network.
-    Args:
-        dZ (np.ndarray): matrix of shape (m, h_new, w_new, c_new) containing
-                         the partial derivatives with respect to the
-                         unactivated output of the convolutional layer.
-        A_prev (np.ndarray): matrix of shape (m, h_prev, w_prev, c_prev)
-                             containing the output of the previous layer.
-        W (np.ndarray): matrix of shape (kh, kw, c_prev, c_new) containing the
-                        kernels for the convolution.
-        b (np.ndarray): matrix of shape (1, 1, 1, c_new) containing the biases
-                        applied to the convolution.
-        padding (str): same or valid, indicating the type of padding used.
-        stride (tuple): (sh, sw) containing the strides for the convolution.
-    Returns:
-        The partial derivatives with respect to the previous layer (dA_prev),
-        the kernels (dW), and the biases (db), respectively.
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, c_prev, c_new = W.shape
